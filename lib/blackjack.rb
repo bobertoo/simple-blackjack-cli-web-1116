c81269b739b594a49160ed3647538012c865a7e1
Vincente Martinez <vinny@vinnymartinez.com>
2016-11-01 21:10:36 +0000

    checkmate

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,33 +1,49 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total(cards)
   # code #display_card_total here
+  puts "Your cards add up to #{cards}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  card_total = deal_card
+  card_total += deal_card
+  display_card_total(card_total)
+  card_total
 end
 
-def hit?
+def hit?(card_total)
   # code hit? here
+  prompt_user
+  hitorno = get_user_input
+  if hitorno == 'h'
+    card_total += deal_card
+  end
+  card_total
 end
 
 def invalid_command
@@ -40,5 +56,11 @@ end
 
 def runner
   # code runner here
+  welcome
+  cardz = initial_round
+  until cardz > 21 do
+    cardz = hit?(cardz)
+    display_card_total(cardz)
+  end
+  end_game(cardz)
 end
-    
